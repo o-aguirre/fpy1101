32a5f078e455392aa69ee5c107998265998a6361
Vina del Mar <vina@duoc.acad>
2024-05-06 21:13:00 -0400

    2024-05-06 v1.1

--- actividad2.4/ejercicio_1.py
+++ actividad2.4/ejercicio_1.py
@@ -1,4 +1,4 @@
-num = [1, 2, 3, 4, 5, 10, 23, 44]
+num = []
 cont_impar = 0
 cont_par = 0
 list_impar = []
@@ -8,9 +8,15 @@ for i in num:
     if i % 2 == 1:
         cont_impar += 1
         list_impar.append(i)
-    else:
+    elif i % 2 == 0 and i != 0:
         cont_par += 1
         list_par.append(i)
-        
+
+if len(list_impar) == 0:
+    list_impar = 0
+    
+if len(list_par) == 0:
+    list_par = 0
+    
 print('Total números pares: ', cont_par, 'Números: ', list_par, sep='\n')
 print('Total números impares: ', cont_impar, 'Números: ', list_impar, sep='\n')
